858de99f80120931bcf19e8370febc0af1a224b0
Misha Kostin <nemez31415926@gmail.com>
2024-12-13 16:58:11 +0100

    Unit test was implemented

--- src/main/java/de/ait/Main.java
+++ src/main/java/de/ait/Main.java
@@ -1,7 +1,8 @@
 package de.ait;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static String getGreeting(String name){
+        return "Hello, " + name + "!";
     }
 }
---
+++ src/test/java/de/ait/MainTest.java
@@ -0,0 +1,21 @@
+package de.ait;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+   @Test
+
+   void testShouldGreetingReturnCorrectMessage(){
+       Main main = new Main();
+
+       String greeting = main.getGreeting("Test");
+
+       assertEquals("Hello, Test!", greeting);
+
+
+   }
+
+}
